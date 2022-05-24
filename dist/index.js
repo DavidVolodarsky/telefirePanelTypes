0fc197073f18445d6f228f9b3a8a4d6a90ffc771
David Volodarskyi <davidv@emg-soft.com>
2022-05-24 10:05:23 +0300

    "added dist folder"

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+//======== Interfaces for CONNECT =========
+Object.defineProperty(exports, "__esModule", { value: true });
+// export {
+//     ILoginRequest,
+//     ILoginResponse,
+//     ISetCommunicationReq,
+//     ISetCommunicationRes,
+//     IConnectPanelReq,
+//     IConnectPanelRes,
+//     IConfigurationData7000,
+//     IConfigurationDataSaver,
+//     IConfigurationDataGuard,
+//     IConfigurationDataNewGuard,
+//     IConfigurationData3000,
+//     IGetConfigurationReq,
+//     IGetConfigurationRes,
+//     ISetConfigurationReq,
+//     ISetConfigurationRes,
+// };
