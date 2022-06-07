57353ba29ac76548aeb0fa05f8688e835901372e
Nokola Novikov <nikola.novikov@emg-soft.com>
2022-06-07 15:34:23 +0300

    add export from index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-export {};
+export { ILoginRequest, ILoginResponse, ISetCommunicationReq, ISetCommunicationRes, IConnectPanelReq, IConnectPanelRes, IConfigurationData7000, IConfigurationDataSaver, IConfigurationDataGuard, IConfigurationDataNewGuard, IConfigurationData3000, IGetConfigurationReq, IGetConfigurationRes, ISetConfigurationReq, ISetConfigurationRes, IProject, IListProjectsRequest, IListProjectsResponse } from "./AllInterfaces";
