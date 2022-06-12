bcb92d265c9cd123d45e7844000fe4f5daf0ea9b
David Volodarskyi <davidv@emg-soft.com>
2022-06-12 14:31:43 +0300

    fixed "IGetPCCodes(Req/Res)" (undefined)

--- src/AllInterfaces.ts
+++ src/AllInterfaces.ts
@@ -3,13 +3,13 @@
 //-----GetPCCodes-----
 interface IGetPCCodesReq {
     Name :string,
-    Directory:string,
-    ProjectName:string,
-    FileName:string,
-    Panel :string,
-    Type :string,
-    Password :string,
-    PanelVersion :string,
+    Directory:string|undefined,
+    ProjectName:string|undefined,
+    FileName:string|undefined,
+    Panel :string|undefined,
+    Type :string|undefined,
+    Password :string|undefined,
+    PanelVersion :string|undefined,
 }
 
 interface IGetPCCodesRes {
